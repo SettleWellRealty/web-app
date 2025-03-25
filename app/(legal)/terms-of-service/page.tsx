63a89f78418b67e2cac40abe3eb8514cb8252b2b
SettleWell Realty <developer@settlewellrealty.com>
2025-03-24 18:52:38 -0600

    Update page.tsx

--- app/(legal)/terms-of-service/page.tsx
+++ app/(legal)/terms-of-service/page.tsx
@@ -37,7 +37,7 @@ export default function TermsOfService() {
 									</p>
 									<p className="text-muted-foreground text-sm">
 										{Brand.licenseNumber}
-									</p>
+									</p>	100101869
 								</div>
 							</div>
 
